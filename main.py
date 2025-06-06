758970b51e2fac8d15abc77a9daee322942479ad
Aug-Den <augustin.dendievel@gmail.com>
2025-06-06 13:39:56 +0200

    feat: :sparkles: Create trainer_controller and funct add_trainer()

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from schemas import inscription_schemas as insc_sch
 
 # crud
 from crud import learner_controller as learn_contr
+from crud import trainer_controller as train_contr
 
 
 def connect_to_session():
@@ -25,8 +26,8 @@ def connect_to_session():
 
 
 def main():
-    # Création d'un nouvel utilisateur
-    new_user = learn_sch.LearnerCreate(
+    # Users creation
+    new_learner = learn_sch.LearnerCreate(
         name="John",
         firstname="Doe",
         email="john.doe@generator.com",
@@ -35,7 +36,21 @@ def main():
         phone="+213676424242",
         platform_registration_date=date.datetime.now().date(),
     )
-    learn_contr.add_learner(new_user, connect_to_session())
+
+    new_trainer = train_sch.TrainerCreate(
+        name="John",
+        firstname="Doe",
+        email="john.doe@generator.com",
+        birth_date=date.datetime(2009, 6, 6),
+        date_create=date.datetime.now().date(),
+        speciality="DevOps",
+        date_hire=date.datetime(2003, 4, 13),
+        hourly_rate=14.67,
+    )
+
+    # Users insert
+    learn_contr.add_learner(new_learner, connect_to_session())
+    train_contr.add_trainer(new_trainer, connect_to_session())
 
 
 if __name__ == "__main__":
