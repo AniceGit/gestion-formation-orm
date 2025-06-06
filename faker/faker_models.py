8dd246d1609bb942f8c5661b11f71709dc84378d
Romain Guirous <romaing_59@hotmail.fr>
2025-06-06 10:03:55 +0200

    perf: :art: improvement faker (not finished)

--- faker/faker_models.py
+++ faker/faker_models.py
@@ -1,5 +1,14 @@
 import faker as fk
-from config import NBR_ADMIN,NBR_INSCRIPTION,NBR_LEARNER,NBR_ROOM,NBR_SESSION,NBR_TEACHINGSTAFF,NBR_TRAINER
+from config import (
+    NBR_ADMIN,
+    NBR_INSCRIPTION,
+    NBR_LEARNER,
+    NBR_ROOM,
+    NBR_SESSION,
+    NBR_TEACHINGSTAFF,
+    NBR_TRAINER,
+    NBR_USER,
+)
 from models.admin import Admin
 from models.inscription import Inscription
 from models.learner import Learner
@@ -11,142 +20,152 @@ from models.user import User
 
 faker = fk.Faker()
 
+
 # function used several times
 def random_element_list(liste):
-    return liste[faker.pyint(0, len(liste) -1)]
+    return liste[faker.pyint(0, len(liste) - 1)]
 
 
-#region admin
+# region admin
+
 
 def generate_fake_admin(nb_admins: int = NBR_ADMIN) -> list[Admin]:
     list_result = []
     for _ in range(nb_admins):
         admin = Admin(
-            id_user = faker.pyint(),
+            id_user=faker.pyint(1, NBR_USER),
             # access_level = ,
-            promotion_date = faker.date()
+            promotion_date=faker.date(),
         )
         list_result.append(admin)
     return list_result
 
 
-#region inscription
+# region inscription
 
-def generate_fake_inscription(nb_inscriptions: int = NBR_INSCRIPTION) -> list[Inscription]:
+
+def generate_fake_inscription(
+    nb_inscriptions: int = NBR_INSCRIPTION,
+) -> list[Inscription]:
     list_result = []
     list_status = ["ENREGISTRE", "DESINSCRIT", "EN_ATTENTE"]
     for _ in range(nb_inscriptions):
         inscription = Inscription(
-            inscription_date = faker.date(),
-            inscription_status = random_element_list(list_status),
-            presence = faker.pyint(0,1),
-            id_session = faker.pyint(), 
-            id_learner = faker.pyint(),
+            inscription_date=faker.date(),
+            inscription_status=random_element_list(list_status),
+            presence=faker.pyint(0, 1),
+            id_session=faker.pyint(1, NBR_SESSION),
+            id_learner=faker.pyint(1, NBR_LEARNER),
         )
         list_result.append(inscription)
     return list_result
 
 
-#region learner
+# region learner
 
-def generate_fake_learner(nb_learners):
+
+def generate_fake_learner(nb_learners=NBR_LEARNER):
     list_result = []
     for _ in range(nb_learners):
         learner = Learner(
-            id_user = faker.pyint(),
-            birth_date = faker.date(),
-            study_level = faker.pyint(0,1),
-            phone = faker.phone_number(),
-            platform_registration_date = faker.date(),
+            id_user=faker.pyint(1, NBR_USER),
+            birth_date=faker.date(),
+            study_level=faker.pyint(0, 1),
+            phone=faker.phone_number(),
+            platform_registration_date=faker.date(),
         )
         list_result.append(learner)
     return list_result
 
 
-#region room
+# region room
 
-def generate_fake_room(nb_rooms):
+
+def generate_fake_room(nb_rooms=NBR_ROOM):
     list_result = []
     for _ in range(nb_rooms):
         room = Room(
-            name = faker.name(),
-            capacity = faker.pyint(),
-            localization = faker.building_name(),
-            stuff = faker.sentence(4, True),
-            is_active = faker.pyint(0,1),
+            name=faker.name(),
+            capacity=faker.pyint(),
+            localization=faker.building_name(),
+            stuff=faker.sentence(4, True),
+            is_active=faker.pyint(0, 1),
         )
         list_result.append(room)
     return list_result
 
 
-#region session
+# region session
 
-def generate_fake_session(nb_sessions):
+
+def generate_fake_session(nb_sessions=NBR_SESSION):
     list_result = []
     list_status = ["OPEN", "CLOSED", "ARCHIVED"]
     for _ in range(nb_sessions):
         session = Session(
-            title = faker.sentence(5, True),
-            description = faker.sentence(14, True),
-            start_date = faker.date(),
-            end_date = faker.date(),
-            max_capacity = faker.pyint(),
-            status = random_element_list(list_status),
-            requirements = ,
-            id_trainer = faker.pyint(),
-            id_room = faker.pyint(),
+            title=faker.sentence(5, True),
+            description=faker.sentence(14, True),
+            start_date=faker.date(),
+            end_date=faker.date(),
+            max_capacity=faker.pyint(),
+            status=random_element_list(list_status),
+            # requirements = ,
+            id_trainer=faker.pyint(1, NBR_TRAINER),
+            id_room=faker.pyint(1, NBR_ROOM),
         )
         list_result.append(session)
     return list_result
 
 
-#region teachin
+# region teachin
 
-def generate_fake_teachingstaff(nb_teachingstaff):
+
+def generate_fake_teachingstaff(nb_teachingstaff=NBR_TEACHINGSTAFF):
     list_result = []
     list_role = ["RESPONSABLE PEDAGOGIQUE", "RESPONSABLE PEDAGOGIQUE"]
     for _ in range(nb_teachingstaff):
         teachingStaff = TeachingStaff(
-            id_user = faker.pyint(),
-            work = random_element_list(list_role),
-            date_appointement = faker.date(),
-            responsabilities = faker.
+            id_user=faker.pyint(1, NBR_USER),
+            work=random_element_list(list_role),
+            date_appointement=faker.date(),
+            # responsabilities = faker.
         )
         list_result.append(teachingStaff)
     return list_result
 
 
-#region trainer
+# region trainer
 
-def generate_fake_trainer(nb_trainers):
+
+def generate_fake_trainer(nb_trainers=NBR_TRAINER):
     list_result = []
     for _ in range(nb_trainers):
         trainer = Trainer(
-            id_user = faker.pyint(),
-            speciality = faker.word(),
-            date_hire = faker.date(),
-            hourly_rate = faker.pyfloat(),
-            bio = faker.sentence(10, True),
+            id_user=faker.pyint(1, NBR_USER),
+            speciality=faker.word(),
+            date_hire=faker.date(),
+            hourly_rate=faker.pyfloat(),
+            bio=faker.sentence(10, True),
         )
         list_result.append(trainer)
     return list_result
 
 
-#region user
+# region user
 
-def generate_fake_user(nb_users):
+
+def generate_fake_user(nb_users=(NBR_USER)):
     list_result = []
     list_user_role = ["Learner", "Trainer", "TeachingStaff", "Admin"]
     for _ in range(nb_users):
         user = User(
-            name = faker.name(),
-            firstname = faker.first_name(),
-            email = faker.email(),
-            age = faker.pyint(18, 79),
-            date_create = faker.date(),
-            is_active = faker.pyint(0,1),
-            role = random_element_list(list_user_role),
+            name=faker.name(),
+            firstname=faker.first_name(),
+            email=faker.email(),
+            age=faker.pyint(18, 79),
+            date_create=faker.date(),
+            is_active=faker.pyint(0, 1),
+            role=random_element_list(list_user_role),
         )
         list_result.append(user)
     return list_result
-    
