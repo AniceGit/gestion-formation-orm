0f9c609f5a5a059fe6657bfcdfdc2d14cdea7522
Aug-Den <augustin.dendievel@gmail.com>
2025-06-10 14:14:37 +0200

    fix: :bug: Fix unique AdminRole

--- models/admin.py
+++ models/admin.py
@@ -1,4 +1,5 @@
 from sqlmodel import Field, SQLModel
+from sqlalchemy import UniqueConstraint
 from typing import Optional
 from datetime import date
 import models.user as u
@@ -12,6 +13,7 @@ class AdminAdminRoleLink(SQLModel, table=True):
 
 class AdminRole(SQLModel, table=True):
     __tablename__ = "admin_role"
+    __table_args__ = (UniqueConstraint("name"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str  # e.g. "SUPERADMIN", "ADMIN_STANDARD"
 
